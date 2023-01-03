16aaae79610a89c0207f57e410c98aee17e29065
Daksha Deep <dakshadeep1234@gmail.com>
2023-01-03 20:48:53 +0530

    added gitignore file

--- src/main/kotlin/Classes/Classes1.kt
+++ src/main/kotlin/Classes/Classes1.kt
@@ -2,7 +2,10 @@ package Classes
 class Employee(
     val name: String,
     var Possition: String
-)
+){
+    var label : String = "Label"
+    // Remember if you define a property inside a class then you have to initialize it.
+}
 fun main(){
     val employee1 = Employee("alex","manager")
     val employee2 = Employee("bob","SDE1")
@@ -11,8 +14,11 @@ fun main(){
     println(employee1.name)
 
     // We can also change the values in the class using the mentioned property
-    employee2.Possition = "SDE2"
+    employee2.Possition = "SDE2" //Here the '.' is called a constructor.
     println(employee2.Possition)
-
+    println(employee2.label)
+    employee2.label = "New Label"
+    println(employee2.label)
+    
 
 }
