5cd6696a961845bb658fa7ccb89cae143fea3709
Daksha Deep <dakshadeep1234@gmail.com>
2023-01-03 21:06:25 +0530

    file addition

---
+++ src/main/kotlin/Classes/ModelingTheWorld.kt
@@ -0,0 +1,15 @@
+package Classes
+
+class Student(
+    val Name : String,
+    val Birthday : String,
+    val Status : String
+)
+
+class Teacher(val Name: String, val Surname : String, val Birthday: String, val Status : String
+)
+
+fun main(){
+    val SangitaChopra = Teacher(Name = "Sangita", Surname = "Chopra", Birthday = "23.08.1991", Status = "Active")
+    println(SangitaChopra.Name)
+}
---
+++ src/main/kotlin/Methods/AboutMethods.kt
@@ -0,0 +1,15 @@
+package Methods
+
+// Methods are nothing but the further property which we define with our class
+
+class Student1(
+    val name:String,
+    val age:Int,
+    val birthday:String
+){
+    //here is the method
+    fun Status(){
+        println("Playing game")
+    }
+    // here we further explained out method
+}
