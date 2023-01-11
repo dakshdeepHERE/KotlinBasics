05bcdf3597e0bc3880dcd9a8b37622b5ae712a4a
Daksha Deep <dakshadeep1234@gmail.com>
2023-01-11 22:08:36 +0530

    changes

--- src/main/kotlin/Basics /Loops.kt
+++ src/main/kotlin/Basics /Loops.kt
@@ -1,11 +1,15 @@
-package `Basics `
+import kotlin.reflect.KProperty
 
-fun main(){
-    var number = 1
+class Example {
+    var prop: String by Delegate()
+}
 
-    while (number<=100){
-        print("$number ")
-        number *=2
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
     }
 
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name}' in $thisRef.")
+    }
 }
