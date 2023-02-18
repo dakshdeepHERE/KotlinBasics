218b1e4954f7c20ab0c609f8fd0fe3eed5997449
Daksha Deep <dakshadeep1234@gmail.com>
2023-02-18 22:29:47 +0530

    interface

--- src/main/kotlin/OOPs/Interface2.kt
+++ src/main/kotlin/OOPs/Interface2.kt
@@ -1,10 +1,31 @@
 package OOPs
 
 fun main(){
-
+    val Sheero = dog("Sheero","German Shepherd")
+    println(Sheero.name)
+    println(Sheero.breed)
+    println(Sheero.pet())
+    Sheero.breed = "Not German" //Open in nature
+    println(Sheero.breed)
 }
 
 interface Animal {
-    val breed:String
+    var breed:String
     fun pet()
+}
+class cat(//Arguments for the class cat
+    val name:String,
+    override var breed: String
+) : Animal{//instantiating the interface
+    override fun pet() {
+        println("Meow")
+    }
+}
+class dog(//arguments
+    val name:String,
+    override var breed: String
+):Animal{//instantiating the interface
+    override fun pet() {
+        println("Bark")
+    }
 }
