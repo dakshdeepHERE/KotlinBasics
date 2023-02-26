389430e9045035c3b1561a5d66804ac8db578e3d
Daksha Deep <dakshadeep1234@gmail.com>
2023-02-26 22:23:56 +0530

    added table info for abstract and interface

---
+++ src/main/kotlin/OOPs/Interface_vs_Abstract.md
@@ -0,0 +1,11 @@
+# Abstract vs Interface
+
+| Interface                                     | Abstract                                             |
+|-----------------------------------------------|------------------------------------------------------|
+| All methods are open                          | Methods can be open or non open                      |
+| A class can implement one or more interface   | Methods and members can be abstract or non abstract  |
+ | A class can implement one or more interface   | A class can only be inherited  from one class        |
+
+## Concluding thoughts
+
+We now know more about abstract classes. We considered their restrictions and explored how abstract classes are used in practice – such as in the above files of abstract and interface files.
--- src/main/kotlin/OOPs/Modifier_Private.kt
+++ src/main/kotlin/OOPs/Modifier_Private.kt
@@ -2,14 +2,19 @@ package OOPs
 
 open class BankAcoount(){
     private var bank_balance:Int = 0
+    protected var bank_Name:String="SBI"
 
     fun bankBalance(){
         println(bank_balance) // you can see here we called bank_balance and no error is there
+        println(bank_Name)
     }
 }
 
 open class Salary constructor(ammount:Int) :BankAcoount(){
     var ammount = 0
+    fun printName(){
+        println(bank_Name)
+    }
     // val NewBank_balance= bank_balance + ammount
     // error will be shown above line as the variable bank_balance is a private variable which can only be
     // used in the same function or class
@@ -19,5 +24,5 @@ class Spending(MoneySpent:Int, ammount: Int): Salary(ammount){
     var MoneySpent = 0
     // val MoneyLeft = NewBank_balance - MoneySpent
     // fun MoneyLefT(){
-        // return MoneyLeft
+         // return MoneyLeft
     }
